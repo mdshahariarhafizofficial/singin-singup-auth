03a22bcb503d2443f01e3f8a163f4cdad397c699
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-04-30 14:33:41 +0600

    Login Form added

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link, NavLink } from "react-router";
+
+const Login = () => {
+  return (
+    <div className="bg-linear-65 from-purple-500 pt-20 to-pink-500 h-screen">
+
+      <form className="max-w-sm mx-auto bg-white p-5 space-y-4 rounded-xl">
+        <div>
+            <h2 className="text-center text-3xl font-bold">Login Now</h2>
+            <div className="my-5 flex gap-0">
+                <NavLink to="/login" type="button" className={
+                    ({isActive})=> isActive? "w-full text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-sm text-sm px-5 py-2.5 text-center mb-2": "w-full text-black border-2 border-gray-200 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-sm text-sm px-5 py-2.5 text-center mb-2"
+                }>Login</NavLink>
+                <NavLink to="/register" type="button" className={
+                    ({isActive})=> isActive? "w-full text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-sm text-sm px-5 py-2.5 text-center mb-2": 
+                    "w-full text-black border-2 border-gray-200 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-sm text-sm px-5 py-2.5 text-center mb-2 border-l-0"
+                }>Register</NavLink>
+            </div>
+        </div>
+
+        {/* Email */}
+        <label className="w-full input validator">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+            </g>
+          </svg>
+          <input type="email" placeholder="mail@site.com" required />
+        </label>
+        <div className="validator-hint hidden">Enter valid email address</div>
+
+        {/* Password */}
+        <label className="w-full input validator">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+              <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+            </g>
+          </svg>
+          <input
+            type="password"
+            required
+            placeholder="Password"
+            minLength="8"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+          />
+        </label>
+        
+        {/* Forgot Password */}
+        <p className="text-pink-500">Forgot Password?</p>
+
+        {/* Button */}
+        <button type="button" className="w-full text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-sm text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
+
+        <p className="text-center my-4">Not a member? <Link to="/register" className="text-pink-500 font-semibold">Register Now!</Link></p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './Pages/Home/Home.jsx'
+import Root from './Layouts/Root.jsx'
+import Login from './Pages/Login/Login.jsx'
+import Register from './Pages/Register/Register.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: '/login',
+        Component: Login
+      },
+      {
+        path: '/register',
+        Component: Register
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
