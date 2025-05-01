8b17245bfb228079eaf0164d078111ad37eb211e
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-01 21:42:05 +0600

    side effect handle using useEffect

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,7 +1,11 @@
+import { use } from "react";
 import { NavLink } from "react-router";
+import { AuthContext } from "../../Contexts/AuthContext";
 
 const Navbar = () => {
-
+  const {user} = use(AuthContext)
+  console.log(user);
+  
   const links = (
     <>
       <li>
@@ -67,7 +71,11 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Contact Us</button>
+        <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+          {
+            user? "Log Out" : "LogIn"
+          }
+        </button>
       </div>
     </div>
   );
--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../Firebase/firebase_init';
 
 const AuthProvider = ({children}) => {
-
+    const [user, setUser] = useState(null);
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
@@ -12,17 +12,27 @@ const AuthProvider = ({children}) => {
         return signInWithEmailAndPassword(auth, email, password)
     }
 
-    onAuthStateChanged(auth, (currentUser)=>{
-        if (currentUser) {
-            console.log("User Found :", currentUser);
+    // onAuthStateChanged(auth, (currentUser)=>{
+    //     if (currentUser) {
+    //         console.log("User Found :", currentUser);
             
-        }else{
-            console.log("User Not Found :", currentUser);
-        }
-    })
+    //     }else{
+    //         console.log("User Not Found :", currentUser);
+    //     }
+    // })
 
+    useEffect( ()=>{
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log("inside useEffect ", currentUser);
+            setUser(currentUser)
+        })
+        return () => {
+            unSubscribe();
+        }
+    }, [])
 
     const userInfo = {
+        user,
         createUser,
         logInUser,
     }
